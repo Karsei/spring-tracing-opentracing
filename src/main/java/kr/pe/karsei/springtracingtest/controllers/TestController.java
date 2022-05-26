5e22d5d3081646c0486ffe6af1d3b1ee98ba6424
Kai <leejy@gabia.com>
2022-05-26 09:38:34 +0900

    feat: api 호출 추가

--- src/main/java/kr/pe/karsei/springtracingtest/controllers/TestController.java
+++ src/main/java/kr/pe/karsei/springtracingtest/controllers/TestController.java
@@ -1,19 +1,41 @@
 package kr.pe.karsei.springtracingtest.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "api1")
 @RequiredArgsConstructor
 public class TestController {
     private final RestTemplate restTemplate;
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello() {
+        log.info("called hello method");
         return "hello, world!";
+    }
+
+    @GetMapping("call")
+    public String call() {
+        log.info("called call method");
+        String url = "http://localhost:7000/api1/hello";
+
+        StringBuilder sb = new StringBuilder();
+        try {
+            log.info("sending");
+            sb.append(restTemplate.getForObject(url, String.class));
+        }
+        catch (HttpStatusCodeException e) {
+            throw e;
+        }
+        log.info("sending done");
+
+        return String.format("call - %s", sb);
     }
 }
